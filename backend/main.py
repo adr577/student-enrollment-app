3e2527f5130598f4e24c5586b67f81e0f0816bad
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-04-09 12:03:05 -0700

    now our backend will serve our build react app

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 #NOTE: Look at the docker compose file for the database connection string. if you are on windows, and can't connect, change 0.0.0.0 to localhost
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@0.0.0.0:5432/mydatabase'
 db = SQLAlchemy(app)
 
 
 
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
