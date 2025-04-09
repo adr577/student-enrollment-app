4ba85313d2939b0ef233be34762ad0fdb79e8ebc
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-04-09 11:52:02 -0700

    added docker compose file, and set up db object in main.py

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+#NOTE: Look at the docker compose file for the database connection string. if you are on windows, and can't connect it change 0.0.0.0 to localhost
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@0.0.0.0:5432/mydatabase'
+db = SQLAlchemy(app)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+
+
 
 
 if __name__ == '__main__':
