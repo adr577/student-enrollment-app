1c8c97c4c38ebd94ed11d36c629cb62c92239a65
adr577 <adrian003hernandez@gmail.com>
2025-04-15 17:19:59 -0700

    added changesh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
   plugins: [react()],
-=======
-=======
->>>>>>> Stashed changes
+
   base: './', // ← VERY IMPORTANT so paths work when Flask serves it
   plugins: [react(), tailwindcss()],
   server: {
@@ -22,8 +19,5 @@ export default defineConfig({
   build: {
     outDir: 'dist', // this is default, but good to be explicit
   },
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
 })
